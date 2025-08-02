ed06189f5364c0667318a8110567077e13150e1a
Keylor Gómez <key.go.1101@gmail.com>
2025-08-02 15:58:40 -0600

    feat(DRO-7): Create landing dropping base page

--- frontend/src/components/TypewriterBanner.jsx
+++ frontend/src/components/TypewriterBanner.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Typewriter } from "react-simple-typewriter";
 
 export default function TypewriterBanner() {
+    const { t: informationCardTranslate } = useTranslation("informationCardFlow");
   return (
      <div className="max-w-3xl mx-auto mt-8 bg-[#fdf8ee] rounded-2xl border border-[#f0e3d0] shadow-[0_8px_30px_rgba(0,0,0,0.08)] hover:shadow-[0_12px_36px_rgba(0,0,0,0.12)] transition-shadow duration-300 px-4 py-6 text-center">
       
       <span className="text-[#975023] text-xl md:text2xl font-bold">
-        Are you 👀 for something different?
+        {informationCardTranslate("primaryInfo")}
       </span>
 
       <div className="text-[#6d6767] text-xl md:text-2xl font-semibold italic h-[40px] mt-2">
         <Typewriter
-          words={["Vintage fits", "Global drips", "Limited drops?"]}
+          words={[informationCardTranslate("phrase1"), informationCardTranslate("phrase2"), informationCardTranslate("phrase3")]}
           loop={0}
           cursor
           cursorStyle="|"
@@ -22,7 +24,7 @@ export default function TypewriterBanner() {
       </div>
 
       <span className="text-[#975023] text-xl md:text-xl font-semibold">
-        New drop coming soon. 🔜
+        {informationCardTranslate("nextNewDrop")}
       </span>
 
     </div>
